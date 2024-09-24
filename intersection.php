f03c55ccae9ff9570361107cc85dc511fc2a90cd
theo <37940969+theo52130@users.noreply.github.com>
2024-09-24 16:51:50 +0200

    up

--- intersection.php
+++ intersection.php
@@ -1,11 +1,19 @@
 <?php
-if ($_SESSION['role'] == 'admin') {
-    header("Location: welcome-admin.php");
-    exit();
-} elseif ($_SESSION['role'] == 'employer') {
-    header("Location: welcome-employer.php");
-    exit();
-} elseif ($_SESSION['role'] == 'client') {
-    header("Location: welcome-client.php");
-    exit();
+session_start();
+
+if (isset($_SESSION['role'])) {
+    if ($_SESSION['role'] == 'admin') {
+        header("Location: welcome-admin.php");
+        exit();
+    } elseif ($_SESSION['role'] == 'employer') {
+        header("Location: welcome-employer.php");
+        exit();
+    } elseif ($_SESSION['role'] == 'client') {
+        header("Location: welcome-client.php");
+        exit();
+    } else {
+        echo "Rôle non reconnu.";
+    }
+} else {
+    echo "Session non démarrée ou rôle non défini.";
 }
