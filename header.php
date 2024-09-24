d4cc4c65114d3fd4558b3207a5dc32f4dc795629
theo <37940969+theo52130@users.noreply.github.com>
2024-09-24 16:33:56 +0200

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-
     <title>Header</title>
 </head>
 
@@ -14,8 +13,16 @@
         <nav>
             <ul>
                 <li><a href="welcome.php">Accueil</a></li>
-                <li><a href="login.php">Connexion</a></li>
-                <li><a href="register.php">Inscription</a></li>
+                <?php
+                if (!isset($_SESSION['username'])) { ?>
+                    <li><a href="login.php">Connexion</a></li>
+                    <li><a href="register.php">Inscription</a></li>
+                <?php
+                } else { ?>
+                    <li><a href="logout.php">Déconnexion</a></li>
+                <?php
+                }
+                ?>
             </ul>
         </nav>
     </header>
