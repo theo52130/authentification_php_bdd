062e3052ac8746e229416028e955c9d54d404d82
theo <37940969+theo52130@users.noreply.github.com>
2024-09-24 16:49:18 +0200

    up

--- intersection.php
+++ intersection.php
@@ -1,8 +1,11 @@
 <?php
 if ($_SESSION['role'] == 'admin') {
     header("Location: welcome-admin.php");
+    exit();
 } elseif ($_SESSION['role'] == 'employer') {
     header("Location: welcome-employer.php");
+    exit();
 } elseif ($_SESSION['role'] == 'client') {
     header("Location: welcome-client.php");
+    exit();
 }
